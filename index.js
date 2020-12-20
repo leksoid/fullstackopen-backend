f25aaf1164da1f9ea4260cae9abe999bf7953360
leksoid <aliaksei.taran@gmail.com>
2020-12-19 22:55:19 -0500

    Add static files for frontend

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-data'))
 app.use(cors())
+app.use(express.static('build'))
 
 const generateId = () => {
     const id = Math.random() * (10 ** 20)
